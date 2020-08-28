dc083773f395cd2f3ed7ec0f382acc0e6a302356
MercerJR <973570588@qq.com>
2020-08-28 09:50:02 +0800

    add Eureka Cluster

---
+++ cloud-eureka-server-7002/src/main/java/com/train/springcloud/EurekaMain7002.java
@@ -0,0 +1,17 @@
+package com.train.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: Mercer JR
+ * @Date: 2020/8/28 9:43
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
